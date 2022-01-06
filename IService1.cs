30e83582f7490045625807abd2c5b3387f89d6e7
junaidAhmed8819 <junaidahmed882000@gmail.com>
2022-01-06 09:51:36 +0530

    Add project files.

---
+++ IService1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JunaidAhmed_20181COM0065_7COM1
+{
+    
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        double ShowCurrencyRate(string foriegnCurrency);
+
+    }
+
+
+    [DataContract]
+    public class CurrencyExchange
+    {
+        string foriCurrency;
+        double indianCurrency;
+
+        [DataMember]
+        public string ForiCurrency
+        {
+            get { return foriCurrency; }
+            set { foriCurrency = value; }
+        }
+
+        [DataMember]
+        public double IndiCurrency
+        {
+            get { return indianCurrency; }
+            set { indianCurrency = value; }
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace JunaidAhmed_20181COM0065_7COM1
+{
+    
+    public class Service1 : IService1
+    {
+        public double ShowCurrencyRate(string foriCurrency)
+        {
+            double ans = 0;
+
+            SqlConnection scon = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\91879\source\repos\JunaidAhmed_20181COM0065_7COM1\App_Data\Database1.mdf;Integrated Security=True");
+
+            SqlCommand scom = new SqlCommand("", scon);
+            scom.CommandText = "Select * from CurrencyExchangeOffice where ForiegnCurrency=@foriCurrency";
+            scom.Parameters.Add("@foriCurrency", System.Data.SqlDbType.VarChar).Value = foriCurrency;
+
+            scon.Open();
+
+            SqlDataReader reader = scom.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    CurrencyExchange ce = new CurrencyExchange();
+                    ce.IndiCurrency = reader.GetDouble(1);
+                    ans = ce.IndiCurrency;
+                }
+            }
+            else
+            {
+                //No Record
+            }
+            reader.Close();
+            return ans;
+        }
+
+        
+    }
+}
+
